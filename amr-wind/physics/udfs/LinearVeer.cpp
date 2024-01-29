32963a6d8b071403cc615cbdd8dfb27bceb45e80
Jaime Liew <jaimeliew1@gmail.com>
2024-01-29 16:23:08 -0500

    add veer functionality

--- amr-wind/physics/udfs/LinearVeer.cpp
+++ amr-wind/physics/udfs/LinearVeer.cpp
@@ -10,25 +10,13 @@ namespace amr_wind::udf {
 
 LinearVeer::LinearVeer(const Field& fld)
 {
-    // This is a where the user can set some user defined variables
-    // This capability can be activated with the following in the input file:
-    // xlo.type = "mass_inflow"
-    // xlo.velocity.inflow_type = LinearVeer
-    // LinearVeer.foo = 1.0
 
-    // clang-format off
-    //{
        const int ncomp = fld.num_comp();
        amrex::ParmParse pp("LinearVeer");
-       pp.query("foo", m_op.foo);
-    //    amrex::Vector<amrex::Real> vel(0.0, ncomp);
-    //    pp.getarr("velocity", vel);
-    //    AMREX_ALWAYS_ASSERT(vel.size() == ncomp);
-    //    for (int i = 0; i < ncomp; ++i) {
-    //        m_op.bar[i] = vel[i];
-    //    }
-    //}
-    // clang-format on
+       pp.query("amplitude", m_op.amplitude);
+       pp.query("Lz", m_op.Lz);
+       pp.query("D", m_op.D);
+       pp.query("Uinf", m_op.Uinf);
 
 }
 
